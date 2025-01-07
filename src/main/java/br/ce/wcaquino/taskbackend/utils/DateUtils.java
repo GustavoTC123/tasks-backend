97f77c3ebce858cefe980f930597202360f7c043
Your NameGustavoTC123 <gustavowebertc@gmail.com>
2025-01-07 11:27:44 -0300

    Tornando classe DateUtils abstrata

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -2,7 +2,7 @@ package br.ce.wcaquino.taskbackend.utils;
 
 import java.time.LocalDate;
 
-public class DateUtils {
+public abstract class DateUtils {
 
 	public static boolean isEqualOrFutureDate(LocalDate date) {
 		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
